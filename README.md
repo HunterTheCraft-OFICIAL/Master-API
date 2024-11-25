8299dd40642f9cf0e931cc3546be8d7c45276416
HunterTheCraft-OFICIAL <impahunter67@gmail.com>
2024-11-24 22:00:58 -0300

    Dev Back-end #02

--- README.md
+++ README.md
@@ -5,7 +5,12 @@
 
 
 ## No Projeto a Versão Representa o Vídeo #01 da Aula
+
+
 Dev_Back-end_#01_v0.0.1
 
+Desvendado APIs e Servidores 
 https://youtu.be/yqFL6SxTaD4?si=4qw5pMVsSmkE1GAs
+
+## No projeto a Versão Representa o Vídeo #02 da Aula
 
--- server.js
+++ server.js
@@ -1,27 +1,32 @@
 import express from "express";
 
-const posts = [
-    {
+const posts = [{
+        id: 1,
         Descrição: "Uma foto teste",
         imagem: "https://placecats.com/millie/300/150"
     },
     {
+        id: 2,
         Descrição: "Paisagem montanhosa",
         imagem: "https://picsum.photos/seed/picsum/200/300"
     },
     {
+        id: 3,
         Descrição: "Cachorro brincando no parque",
         imagem: "https://source.unsplash.com/random/200x300/?dog,park"
     },
     {
+        id: 4,
         Descrição: "Pratos deliciosos de um restaurante",
         imagem: "https://unsplash.com/photos/food"
     },
     {
+        id: 5,
         Descrição: "Uma cidade vibrante à noite",
         imagem: "https://source.unsplash.com/random/200x300/?city,night"
     },
     {
+        id: 6,
         Descrição: "Uma obra de arte abstrata",
         imagem: "https://picsum.photos/seed/art/200/300"
     }
@@ -30,14 +35,20 @@ const posts = [
 const app = express();
 app.use(express.json());
 app.listen(3000, () => {
-console.log("Servidor Escutando…");
-});
-
-app.get("/api", (req, res) => {
-res.status(200).send("A Torre Eiffel Ilumina à noite, com milhares de luzes cintilando, criando um espetáculo mágico em Paris.");
+    console.log("Servidor Escutando…");
 });
 
 app.get("/posts", (req, res) => {
-res.status(200).json(posts);
+    res.status(200).json(posts);
 });
 
+function buscarPostPorId(id) {
+return posts.findIndex((post) => {
+return post.id === Number(id)
+});
+}
+
+app.get("/posts/:id", (req, res) => {
+ const index = buscarPostPorId(req.params.id)
+    res.status(200).json(posts[index]);
+});
